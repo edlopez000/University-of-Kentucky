ba26ccc56ce0f50885169edc000adbb200414627
Brandon <brandonbgrayson@gmail.com>
2021-11-26 20:53:07 -0500

    Footer UI completed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import { ChakraProvider, Box, Text } from "@chakra-ui/react"
 import theme from './theme';
 import Navbar from "../src/components/Navbar"
 import Home from "../src/components/homePage/Home"
+import Footer from './components/Footer';
 
 function App() {
   return (
     <ChakraProvider theme={theme} >
-      <Box>
+      <Box 
+       d="flex"
+       flexDirection="column"
+       h="100vh"
+       >
         <Navbar />
         <Home />
+        <Footer />
       </Box>
     </ChakraProvider>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,67 @@
-import React from 'react'
+import React from 'react';
+import { Box, Text, HStack, VStack, List, Link, ListItem } from '@chakra-ui/react';
 
 export default function Footer() {
-    return (
-        <h1>Footer</h1>
-    )
+	return (
+		<Box bg="brand.900" mt="35vh" h="10em">
+			<Box w="100%" d="flex" justifyContent="center" pt="1em">
+				<VStack>
+					<Text fontWeight="400" fontSize="4xl">
+						University of Kentucky
+					</Text>
+
+					<Box d="flex" flexDirection="row">
+						<HStack spacing="1em">
+							<Box>
+								<Link>An Equal Opportunity University</Link>
+							</Box>
+							<Box>
+								<Text>|</Text>
+							</Box>
+							<Box>
+								<Link>Accreditation</Link>
+							</Box>
+							<Box>
+								<Text>|</Text>
+							</Box>
+							<Box>
+								<Link>Directory</Link>
+							</Box>
+							<Box>
+								<Text>|</Text>
+							</Box>
+							<Box>
+								<Link>Email</Link>
+							</Box>
+							<Box>
+								<Text>|</Text>
+							</Box>
+							<Box>
+								<Link>Privacy Policy</Link>
+							</Box>
+							<Box>
+								<Text>|</Text>
+							</Box>
+							<Box>
+								<Link>Accessibility</Link>
+							</Box>
+						</HStack>
+					</Box>
+					<Box>
+						<HStack spacing="1em">
+							<Box>
+								<Link>University of Kentucky</Link>
+							</Box>
+							<Box>
+								<Text>|</Text>
+							</Box>
+							<Box>
+								<Link>Lexington, Kentucky</Link>
+							</Box>
+						</HStack>
+					</Box>
+				</VStack>
+			</Box>
+		</Box>
+	);
 }
